8e9ecd58a404b78a7f245b22d285d502f63408d6
R. ALOUI <aloui.radhouane@gmail.com>
2022-05-07 14:25:18 +0100

    Ajouter l'énoncé du programme

--- mainRectangle.cpp
+++ mainRectangle.cpp
@@ -1,3 +1,25 @@
+/**
+ * @file Rectangle.cpp
+ * @author R. ALOUI (isetk.c2i@gmail.com)
+ * @brief Ce programme doit donner la possibiliter de gérer des formes géométriques,
+ *        en particuler les rectangles. Rappelons qu'un rectangles est une forme 
+ *        géométrique caractérisée par une longueur et une largeur. Le programme doit
+ *        permettre :
+ *          * la création de différents types de rectangles ayant différentes dimensions
+ *          * le calcul du périmètre d'un rectangle
+ *          * le calcul de l'aire d'une rectangle
+ *          * la construction d'un rectangle dont les dimensions sont la 
+ *    			  somme d'autres rectangles
+ *          * la construction d'un rectangle dont les dimensions sont la
+ *    			  différence des autres rectangles
+ *          * afficher les caractéristiques d'un rectangle
+ * @version 0.1
+ * @date 2022-04-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
 #include <iostream>
 
 using std::cout;
